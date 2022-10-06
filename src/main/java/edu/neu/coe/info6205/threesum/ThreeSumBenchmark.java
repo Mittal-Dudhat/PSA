bd6e0ebcf4c31ea641a4b1a006d1cd23c75806ab
Mittal-Dudhat <dudhat.m@northeastern.edu>
2022-10-05 20:49:14 -0400

    Assignment 2

---
+++ src/main/java/edu/neu/coe/info6205/threesum/ThreeSumBenchmark.java
@@ -0,0 +1,82 @@
+package edu.neu.coe.info6205.threesum;
+
+import edu.neu.coe.info6205.util.Benchmark_Timer;
+import edu.neu.coe.info6205.util.Stopwatch;
+import edu.neu.coe.info6205.util.TimeLogger;
+import edu.neu.coe.info6205.util.Utilities;
+
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+import java.util.function.UnaryOperator;
+
+public class ThreeSumBenchmark {
+    public ThreeSumBenchmark(int runs, int n, int m) {
+        this.runs = runs;
+        this.supplier = new Source(n, m).intsSupplier(10);
+        this.n = n;
+        
+        int[] ints = supplier.get();
+        
+        ThreeSum target1 = new ThreeSumQuadratic(ints);
+        ThreeSum target2 = new ThreeSumQuadraticWithCalipers(ints);
+        ThreeSum target3 = new ThreeSumCubic(ints);
+
+
+        Stopwatch s1 = new Stopwatch();
+        Triple[] triples1 = target1.getTriples();
+        long tim1 = s1.lap();
+
+        Stopwatch s2 = new Stopwatch();
+        Triple[] triples2 = target2.getTriples();
+        long tim2 = s2.lap();
+
+        Stopwatch s3 = new Stopwatch();
+        Triple[] triples3 = target3.getTriples();
+        long tim3 = s3.lap();
+
+        System.out.println("N= "+ints.length);
+        System.out.println(" time taken for : ThreeSumQuadratic : "+ tim1 );
+        System.out.println(" time taken for : ThreeSumQuadraticWithCalipers : "+ tim2 );
+        System.out.println(" time taken for : ThreeSumCubic : "+ tim3 );
+    }
+
+    public void runBenchmarks() {
+        System.out.println("ThreeSumBenchmark: N=" + n);
+        benchmarkThreeSum("ThreeSumQuadratic", (xs) -> new ThreeSumQuadratic(xs).getTriples(), n, timeLoggersQuadratic);
+        benchmarkThreeSum("ThreeSumQuadrithmic", (xs) -> new ThreeSumQuadrithmic(xs).getTriples(), n, timeLoggersQuadrithmic);
+        benchmarkThreeSum("ThreeSumCubic", (xs) -> new ThreeSumCubic(xs).getTriples(), n, timeLoggersCubic);
+    }
+
+    public static void main(String[] args) {
+        new ThreeSumBenchmark(100, 250, 250).runBenchmarks();
+        new ThreeSumBenchmark(50, 500, 500).runBenchmarks();
+        new ThreeSumBenchmark(20, 1000, 1000).runBenchmarks();
+        new ThreeSumBenchmark(10, 2000, 2000).runBenchmarks();
+        new ThreeSumBenchmark(5, 4000, 4000).runBenchmarks();
+        new ThreeSumBenchmark(3, 8000, 8000).runBenchmarks();
+        new ThreeSumBenchmark(2, 16000, 16000).runBenchmarks();
+    }
+
+    private void benchmarkThreeSum(final String description, final Consumer<int[]> function, int n, final TimeLogger[] timeLoggers) {
+        if (description.equals("ThreeSumCubic") && n > 4000) return;
+        // FIXME
+        // END 
+    }
+
+    private final static TimeLogger[] timeLoggersCubic = {
+            new TimeLogger("Raw time per run (mSec): ", (time, n) -> time),
+            new TimeLogger("Normalized time per run (n^3): ", (time, n) -> time / n / n / n * 1e6)
+    };
+    private final static TimeLogger[] timeLoggersQuadrithmic = {
+            new TimeLogger("Raw time per run (mSec): ", (time, n) -> time),
+            new TimeLogger("Normalized time per run (n^2 log n): ", (time, n) -> time / n / n / Utilities.lg(n) * 1e6)
+    };
+    private final static TimeLogger[] timeLoggersQuadratic = {
+            new TimeLogger("Raw time per run (mSec): ", (time, n) -> time),
+            new TimeLogger("Normalized time per run (n^2): ", (time, n) -> time / n / n * 1e6)
+    };
+
+    private final int runs;
+    private final Supplier<int[]> supplier;
+    private final int n;
+}
